8c9bfd87097a790db8c53963436ec303582b4e95
zhong <zhongdu332@gmail.com>
2018-01-06 23:11:06 +0800

    20180106.003

--- 数据库/student_db.sql
+++ 数据库/student_db.sql
@@ -1,84 +1,69 @@
 /*
-Navicat MySQL Data Transfer
-
-Source Server         : local
-Source Server Version : 50718
-Source Host           : localhost:3306
-Source Database       : student_db
-
-Target Server Type    : MYSQL
-Target Server Version : 50718
-File Encoding         : 65001
-
-Date: 2017-12-28 12:31:23
+MySQL Data Transfer
+Source Host: localhost
+Source Database: student_db
+Target Host: localhost
+Target Database: student_db
+Date: 2018/1/6 23:10:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
 -- ----------------------------
--- Table structure for `t_admin`
+-- Table structure for t_admin
 -- ----------------------------
 DROP TABLE IF EXISTS `t_admin`;
 CREATE TABLE `t_admin` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户标识ID',
-  `uName` varchar(20) NOT NULL COMMENT '姓名',
-  `uAccounts` varchar(20) NOT NULL COMMENT '登录账号',
-  `uPassword` varchar(50) NOT NULL COMMENT '登录密码',
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=gbk;
-
--- ----------------------------
--- Records of t_admin
--- ----------------------------
-INSERT INTO `t_admin` VALUES ('1', '系统管理员', 'admin', 'e10adc3949ba59abbe56e057f20f883e');
-
--- ----------------------------
--- Table structure for `t_courses`
--- ----------------------------
-DROP TABLE IF EXISTS `t_courses`;
-CREATE TABLE `t_courses` (
-  `cs_no` varchar(20) NOT NULL COMMENT '课程编号',
-  `cs_name` varchar(20) NOT NULL COMMENT '课程名称',
-  `cs_hour` int(11) DEFAULT NULL COMMENT '课时',
-  `cs_score` int(11) NOT NULL COMMENT '学分',
-  PRIMARY KEY (`cs_no`)
+  `uid` int(11) NOT NULL auto_increment COMMENT '标识ID',
+  `uName` varchar(20) NOT NULL COMMENT '名姓',
+  `uAccounts` varchar(20) NOT NULL COMMENT '账号',
+  `uPassword` varchar(50) NOT NULL default '密码',
+  `uRemark` varchar(50) default NULL COMMENT '注备',
+  PRIMARY KEY  (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
 
 -- ----------------------------
--- Records of t_courses
--- ----------------------------
-
--- ----------------------------
--- Table structure for `t_score`
+-- Table structure for t_score
 -- ----------------------------
 DROP TABLE IF EXISTS `t_score`;
 CREATE TABLE `t_score` (
-  `sc_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '成绩标识ID',
-  `cs_no` varchar(20) NOT NULL COMMENT '课程编号',
-  `stu_no` varchar(20) NOT NULL COMMENT '学号',
-  `sc_score` int(11) NOT NULL COMMENT '成绩',
-  PRIMARY KEY (`sc_id`)
+  `sid` int(11) NOT NULL auto_increment COMMENT '绩成标识ID',
+  `stuNo` varchar(20) NOT NULL COMMENT '学生学号',
+  `course` varchar(20) NOT NULL COMMENT '程课编号 ',
+  `score` int(11) NOT NULL COMMENT '课程成绩',
+  PRIMARY KEY  (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
 
 -- ----------------------------
--- Records of t_score
+-- Table structure for t_student
 -- ----------------------------
-
--- ----------------------------
--- Table structure for `t_students`
--- ----------------------------
-DROP TABLE IF EXISTS `t_students`;
-CREATE TABLE `t_students` (
-  `stu_no` varchar(20) NOT NULL COMMENT '学号',
-  `stu_name` varchar(20) NOT NULL COMMENT '姓名',
-  `stu_sex` char(2) DEFAULT NULL COMMENT '性别',
-  `stu_age` int(11) DEFAULT NULL COMMENT '年龄',
-  `stu_class` varchar(20) DEFAULT NULL COMMENT '班级',
-  `stu_grade` varchar(20) DEFAULT NULL COMMENT '年级',
-  `stu_remark` varchar(50) DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`stu_no`)
+DROP TABLE IF EXISTS `t_student`;
+CREATE TABLE `t_student` (
+  `stuid` int(11) NOT NULL auto_increment COMMENT '生学标识ID',
+  `stuName` varchar(20) NOT NULL,
+  `stuNo` varchar(20) NOT NULL COMMENT '学号',
+  `cno` varchar(20) default NULL COMMENT '级班编号',
+  `gno` varchar(20) default NULL COMMENT '级年编号',
+  `stuAge` int(11) default NULL COMMENT '龄年',
+  `stuAddr` varchar(50) default NULL COMMENT '地址',
+  `stuSex` char(2) NOT NULL COMMENT '性别',
+  PRIMARY KEY  (`stuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
 
 -- ----------------------------
--- Records of t_students
+-- Records 
 -- ----------------------------
+INSERT INTO `t_admin` VALUES ('1', '张三', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '测试');
+INSERT INTO `t_score` VALUES ('1', '111', '语文', '100');
+INSERT INTO `t_score` VALUES ('2', '111', '数学', '110');
+INSERT INTO `t_score` VALUES ('3', '111', '英语', '100');
+INSERT INTO `t_student` VALUES ('1', '耙耳朵', '111', '123', 'd', '1', 'a', '1');
+INSERT INTO `t_student` VALUES ('2', '张三', '222', '124', '22', '23', '22222222111', '1');
+INSERT INTO `t_student` VALUES ('3', '风车车', '333', '125', '11', '22', '1312312313', '1');
+INSERT INTO `t_student` VALUES ('4', '王五', '444', '126', '555', '22', '6666', '1');
+INSERT INTO `t_student` VALUES ('5', '李四', '555', '127', 'ddd', '33', '', '1');
+INSERT INTO `t_student` VALUES ('6', '假老练', '666', '128', '22344', '22', '', '0');
+INSERT INTO `t_student` VALUES ('7', 'aaaa', '433', '444', '55', '12', '2345', '1');
+INSERT INTO `t_student` VALUES ('8', 'fdd', '3241', '123', '1234', '23', '4', '1');
+INSERT INTO `t_student` VALUES ('9', 'qww', '23', '234', '243', '44', '24', '1');
+INSERT INTO `t_student` VALUES ('10', '323445', '2323', '22323', '3223', '23', '23', '1');
+INSERT INTO `t_student` VALUES ('11', '345', '324', '32', '2234', '31', '2345', '1');
